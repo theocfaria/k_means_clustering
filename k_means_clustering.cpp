d7cfde617dde25446754c50166be3fe853860813
Theo Faria <theocarfar@gmail.com>
2025-01-28 15:01:27 -0300

    added centroid inicialization and cluster assigning

--- k_means_clustering.cpp
+++ k_means_clustering.cpp
@@ -4,12 +4,17 @@
 
 #define DIMENSIONS 4
 using namespace std;
+
 struct Point
 {
     float coord[DIMENSIONS]; // 0 = x, 1 = y, 2 = z, 3 = w
 };
+struct Cluster
+{
+    Point centroid;
+};
 
-float defineDistance(Point p1, Point p2)
+float defineDistance(Point p1, Point p2) // defines distance between two points in 4 dimensions; scalable function;
 {
     float distance = 0;
     for(int i = 0; i < DIMENSIONS; i++)
@@ -17,6 +22,30 @@ float defineDistance(Point p1, Point p2)
         distance += pow((p2.coord[i] - p1.coord[i]), 2);
     }
     return sqrt(distance);
+}
+
+void initializeCentroids(Cluster clusters[], Point data[], int k, int dataSize)
+{
+    for(int i = 0; i < k; i++)
+    {
+        clusters[i].centroid = data[rand() % dataSize]; // chooses a random point between 0 and (dataSize - 1)
+    }
+}
+
+int assignCluster(Point p, Cluster clusters[], int k)
+{
+    float minDistance = defineDistance(p, clusters[0]. centroid);
+    int clusterIndex = 0;
+    for(int i = 0; i < k; i++)
+    {
+        float distance = defineDistance(p, clusters[i].centroid);
+        if (distance < minDistance)
+        {
+            minDistance = distance;
+            clusterIndex = i;
+        }
+    }
+    return clusterIndex;
 }
 
 int main()
@@ -29,3 +58,5 @@ int main()
     }
     cout << fixed << setprecision(2) << defineDistance(p1, p2);
 }
+
+
